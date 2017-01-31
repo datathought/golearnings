3ebcaf6a27cc8545ba484941f55ff95851e18400
datathought <matt.l.rodriguez@gmail.com>
2017-01-31 13:05:48 -0800

    added url

--- 16_exercises-solutions/06_10001st-prime/main.go
+++ 16_exercises-solutions/06_10001st-prime/main.go
@@ -37,6 +37,7 @@ func main() {
 }
 
 /*
+https://projecteuler.net/problem=7
 10001st prime
 Problem 7
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
